80da5df91ebe0b34def63c6dc7b653b7a7219c2d
Nybobus <150648614+Nybobus@users.noreply.github.com>
2024-10-22 16:56:44 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+import './Header.jsx'
+
+
+const App = () => {
+  return (
+    <>
+
+    <Wrapper/>
+    <Button/>
+    <Card/>
+    <Section/>
+    <Hero/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
